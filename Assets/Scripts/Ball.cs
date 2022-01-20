6986aa4ee55c19f88d0510a0edc71fe53bef07af
f0kes <f0kesppokes@gmail.com>
2022-01-20 21:15:51 +0200

    init

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using DefaultNamespace.Enums;
+using Structures;
+using Structures.Structures;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+	private float _health;
+		
+	[SerializeField] private List<EnumeratedStat> enumeratedStats;
+	private Rigidbody _rb;
+	public StatDict<BallStat> Stats = new StatDict<BallStat>();
+		
+	[Serializable]
+	public struct EnumeratedStat
+	{
+		public BallStat Name;
+		public Stat Value;
+	}
+
+	private void Awake()
+	{
+		foreach (var stat in enumeratedStats)
+		{
+			Stats.SetStat(stat.Name, stat.Value);
+		}
+
+		_health = Stats[BallStat.Health];
+		_rb = GetComponentInChildren<Rigidbody>();
+	}
+
+	public void Hit(Vector3 hitVector)
+	{
+		_rb.AddForce(hitVector, ForceMode.VelocityChange);
+		_health -= hitVector.magnitude;
+		if (_health <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
